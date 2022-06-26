d075843f9d458bec5abe45f261eafac29af0bf3e
André Buchmann <andy.schliesser@gmail.com>
2022-06-26 22:19:56 +0200

    Init NoTeleport mod for FS22

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# No Teleport
+
+Year 3022. There are stories about ancient people and their rudimentairy transportation methods. Sayings tell, those people drove by car to the destination. Sometimes they event went by foot. How ridiculous is this?
+Welcome in 2022... Teleporting is not yet possible...
+
+
+This is a script mod for Farming Simulator 22.
+
+For Bugreports/Ideas please use: https://github.com/schliesser/fs-noteleport/issues
+
+## Features
+
+- No "visit" buttons in production menu
+- No vehicle Tabbing
---
+++ scripts/main.lua
@@ -0,0 +1,27 @@
+----------------------------------------------------------------------------------------------------
+--[[
+No Teleport for Farming Simulator 2022
+
+Copyright (c) -tinte-, 2022
+
+Author: André Buchmann
+Issues: https://github.com/schliesser/fs-noteleport/issues
+
+Feel free to open a pull reuests for enhancements or bugfixes.
+
+You are not allowed to sell this or a modified version of the mod.
+]] ----------------------------------------------------------------------------------------------------
+NoTeleport = {}
+NoTeleport.name = "NoTeleport"
+
+function initNoTeleport()
+    PlaceableHotspot.getBeVisited = Utils.overwrittenFunction(PlaceableHotspot.getBeVisited, NoTeleport.disableFunction)
+    Enterable.getIsEnterableFromMenu = Utils.overwrittenFunction(Enterable.getIsEnterableFromMenu, NoTeleport.disableFunction)
+end
+
+-- return false to disable overwritten function
+function NoTeleport:disableFunction()
+    return false
+end
+
+initNoTeleport()
