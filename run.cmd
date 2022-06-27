cbd4b1298e858c47ec492ce7323a355a300dab99
André Buchmann <andy.schliesser@gmail.com>
2022-06-27 22:19:48 +0200

    Refactor helpers

--- run.cmd
+++ run.cmd
@@ -1,19 +1,19 @@
 @ECHO off
 
-SET gameProfile="G:\Dokumente\My Games\FarmingSimulator2022\\"
-
 @REM Pack mod
 CALL pack.cmd
 
 IF %ERRORLEVEL% NEQ 0 ( exit 1 )
 
+setlocal
+FOR /F "tokens=*" %%i in ('type .env') do SET %%i
+
 ECHO Copy file to mod folder
 COPY %filename% %gameProfile%mods\
 
 
-ECHO Starting game ...
-@REM start steam://rungameid/1248130
-"C:\Program Files (x86)\Steam\steam.exe" -applaunch 1248130 -cheats -autoStartSavegameId 12
+ECHO Starting game with savegame %savegameId% ...
+%gameExe% -cheats -autoStartSavegameId %savegameId%
 
 ECHO Happy Testing: %modName%
 
@@ -22,3 +22,5 @@ ECHO Happy Testing: %modName%
 @REM -autoStartSavegameId 3 (loads the savegame automatically | *Replace "3" with the savegame ID of your choice)
 @REM -restart (prevents intro videos from playing | will also keep logging the game to the logfile)
 @REM -disableFramerateLimiter (removes the FPS cap | not recommended )
+
+endlocal
